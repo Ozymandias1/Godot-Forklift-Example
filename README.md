7ae9e77e03e098f989d9be99a155d9eabecb3e97
Ozymandias1 <mrbaek86@gmail.com>
2024-07-14 02:50:58 +0900

    테스트용 장애물

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## 참고
 애니메이션
 - https://docs.godotengine.org/en/stable/tutorials/animation/animation_tree.html
+- https://youtu.be/ZQOgD724eMA?si=TLWIta84zc7A_Sqc
 
 ## 리소스 출처
 3DGodotRobot(https://captainripley.itch.io/godot-3d-robot-character)
--- Scripts/CameraController.gd
+++ Scripts/CameraController.gd
@@ -5,15 +5,20 @@ extends Node3D
 @export var follow_target: Node3D
 @export var cam_height: float = 1.325
 
+var camera_local_pos: Vector3
+
 # 시작
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera_local_pos = camera_3d.position
 
 # 업데이트
 func _process(delta):
 	# 위치 업데이트
 	if follow_target:
 		self.global_position = follow_target.global_position + (Vector3.UP * cam_height)
+	
+	camera_3d.position = lerp(camera_3d.position, camera_local_pos, 5.0 * delta)
 	
 	# 지게차 탑승 하차시 roll값이 변경되는 경우가 있으므로 0.0으로 고정시킨다.
 	rotation.z = 0.0
@@ -24,9 +29,9 @@ func _input(event):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
 		
 	if event.is_action("Zoom In"):
-		camera_3d.position.z -= 0.5
+		camera_local_pos.z -= 1.0
 	if event.is_action("Zoom Out"):
-		camera_3d.position.z += 0.5
+		camera_local_pos.z += 1.0
 			
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		global_rotate(Vector3.UP, event.relative.x * -0.001)
