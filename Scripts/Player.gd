bac0d987dc68f551ac27fc1a418e9754250bf59f
Ozymandias1 <mrbaek86@gmail.com>
2024-07-14 01:34:53 +0900

    기본 이동 처리

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -23,15 +23,21 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	_handle_movement(delta)
+	move_and_slide()
+
+# 기본 이동
+func _handle_movement(delta):
+	var input_dir = Input.get_vector("Move Left", "Move Right", "Move Down", "Move Up")
+	var ground_plane = Plane(Vector3.UP)
+	var horizontal_movement_input = camera_3d.global_basis.x * input_dir.x
+	var vertical_movement_input = ground_plane.project(camera_3d.global_basis.y) * input_dir.y
+	
+	var desire_direction = (horizontal_movement_input + vertical_movement_input).normalized()
+	
+	if desire_direction:
+		velocity.x = desire_direction.x * SPEED
+		velocity.z = desire_direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
