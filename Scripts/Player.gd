2b890f227b3062cacc8a1d8dbba1f3ab28f63b45
Ozymandias1 <mrbaek86@gmail.com>
2024-07-14 01:28:34 +0900

    카메라 컨트롤러

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,10 +4,16 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var camera_3d = $CameraPivot/Camera3D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# 스크립트 시작
+func _ready():
+	camera_3d.make_current()
 
+# 물리 업데이트
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
