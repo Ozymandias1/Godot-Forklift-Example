8f91e35292469450797e4dc61683ba9031e9af2a
Ozymandias1 <mrbaek86@gmail.com>
2024-07-20 03:27:59 +0900

    지게차 탑승 처리중

--- Scripts/Forklift.gd
+++ Scripts/Forklift.gd
@@ -6,7 +6,12 @@ extends VehicleBody3D
 var max_rpm = 300
 var max_torque = 100
 
+var is_controllable = false
+
 func _physics_process(delta):
+	if not is_controllable:
+		return
+	
 	steering = lerp(steering, Input.get_axis("Steer Left", "Steer Right") * 0.5, 5 * delta)
 	
 	var acceleration = Input.get_axis("Brake", "Throttle")
@@ -14,3 +19,15 @@ func _physics_process(delta):
 	wheel_fl.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = wheel_fr.get_rpm()
 	wheel_fr.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
+
+# 탑승구역(BoardingArea)에 뭔가 탐지되었을 경우 처리
+func _on_boarding_area_body_entered(body):
+	if body.get_meta("is_player", false):
+		var player = body
+		player.add_available_vehicle(self)
+
+# 탑승구역(BoardingArea)에서 탐지된 객체가 빠져나갈 경우 처리
+func _on_boarding_area_body_exited(body):
+	if body.get_meta("is_player", false):
+		var player = body
+		player.remove_available_vehicle(self)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,12 @@ const JUMP_VELOCITY = 4.5
 
 @onready var camera_3d = $CameraPivot/Camera3D
 @onready var animation_tree = $"3DGodotRobot/AnimationTree"
+@onready var boarding_label = $Boarding_Label
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var vehicle_list: Array[Node3D] = []
 
 # 스크립트 시작
 func _ready():
@@ -36,20 +39,20 @@ func _handle_movement(delta):
 	var ground_plane = Plane(Vector3.UP)
 	var horizontal_movement_input = camera_3d.global_basis.x * input_dir.x
 	var vertical_movement_input = ground_plane.project(camera_3d.global_basis.y) * input_dir.y
-	
+
 	var desire_direction = (horizontal_movement_input + vertical_movement_input).normalized()
-	
+
 	if desire_direction:
 		velocity.x = desire_direction.x * SPEED
 		velocity.z = desire_direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
+
 	# 이동 애니메이션
 	var dirLen = desire_direction.length()
 	animation_tree["parameters/Locomotion/blend_position"] = lerp(animation_tree["parameters/Locomotion/blend_position"], dirLen * (SPEED / BASE_SPEED), 5.0 * delta)
-	
+
 	# 입력 방향에 따른 회전 처리
 	if desire_direction:
 		var target_transform = transform.looking_at(global_position + desire_direction, Vector3.UP, true)
@@ -64,3 +67,15 @@ func _handle_jump_fall_animation():
 			animation_tree.play_fall_animation()
 		else:
 			animation_tree.play_jump_animation()
+
+# 탑승 가능한 탈것 목록에 추가
+func add_available_vehicle(vehicle: Node3D):
+	vehicle_list.append(vehicle)
+	update_boarding_label_visible()
+
+func remove_available_vehicle(vehicle: Node3D):
+	vehicle_list.erase(vehicle)
+	update_boarding_label_visible()
+
+func update_boarding_label_visible():
+	boarding_label.visible = vehicle_list.size() > 0
